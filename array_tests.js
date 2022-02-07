f63eda105b40efa28011858fa0b971365430f1d7
Roger Deakins <patrickw7405@gmail.com>
2022-02-07 17:55:33 +0700

    algorithm try to win

--- array_tests.js
+++ array_tests.js
@@ -1,4 +1,8 @@
-// this file acts as a test for "some" and "every" built-in functions for array
+let arr = [0,1,2,3,4,5,6,7,8]
+
+let y;
+let positionOfI = 0;
+
 let win = [
     [0, 1, 2],
     [3, 4, 5],
@@ -10,9 +14,143 @@ let win = [
     [2, 4, 6]
 ]
 
-let botCollected = [0,6,1,7,8]
+// arr = [0,3,4,6,7,8]
+arr = [0,2,6,7]
 
-let playerCollected = [0,6,4,2]
+let botCollected = [5,4]
 
-botCollected = []
-console.log(botCollected);
+let playerCollected = [1,3,8]
+
+const oneTriggerAway = purpose => {
+    let decision = []
+    if (purpose === 'win the game'){
+        for (u = 0; u < win.length; u++){ 
+            
+            for (j = 0; j < win[u].length; j++){ 
+                
+                for (i = 0; i < botCollected.length; i++){ 
+                    // j < 3 or j < win[u].length
+                    if (win[u][j] === botCollected[i]) {
+                        decision.push(botCollected[i])
+                        positionOfI = win[u].indexOf(botCollected[i])
+                        win[u].splice(positionOfI,1)
+                    }
+                    if (win[u].length === 1) {
+                        decision.forEach(n => {
+                            win[u].push(n)
+                        })
+                        decision = []
+                        if (arr.includes(win[u][0])){
+                            return win[u][0]
+                        } else {
+                            continue
+                        }
+                    } else if (j === 2 && decision.length < 2){
+                        decision.forEach(n => {
+                            win[u].push(n)
+                        })
+                        decision = []
+                    }
+                    // console.log(`${decision} is decision and ${win[u]} is current win pattern and ${j} is j`);
+                };
+
+            }
+        };
+    } else if (purpose === 'defend'){
+        for (u = 0; u < win.length; u++){ 
+            
+            for (j = 0; j < win[u].length; j++){ 
+                
+                for (i = 0; i < playerCollected.length; i++){ 
+                    // j < 3 or j < win[u].length
+                    if (win[u][j] === playerCollected[i]) {
+                        decision.push(playerCollected[i])
+                        positionOfI = win[u].indexOf(playerCollected[i])
+                        win[u].splice(positionOfI,1)
+                    }
+                    if (win[u].length === 1) {
+                        decision.forEach(n => {
+                            win[u].push(n)
+                        })
+                        decision = []
+                        if (arr.includes(win[u][0])){
+                            return win[u][0]
+                        } else {
+                            continue
+                        }
+                    } else if (j === 2 && decision.length < 2){
+                        decision.forEach(n => {
+                            win[u].push(n)
+                        })
+                        decision = []
+                    }
+                    // console.log(`${decision} is decision and ${win[u]} is current win pattern and ${j} is j`);
+                };
+
+            }
+        };
+    }
+};
+
+// console.log(oneTriggerAway());
+let winTg = oneTriggerAway('win the game')
+win = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+let defTg = oneTriggerAway('defend')
+win = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+if (winTg !== undefined) {
+    y = winTg
+    win = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    // yCond = 'ready'
+} else {
+    if (defTg !== undefined) {
+        y = defTg
+        win = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+        // yCond = 'ready'
+
+    } else {
+        y = arr[Math.floor(Math.random()*arr.length)]
+        // yCond = 'ready'
+    }
+}
+console.log(y);
+// console.log(arr[Math.floor(Math.random()*arr.length))
+// console.log(arr);
+
+
+
--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const container = document.querySelector('.container')
 const box = document.querySelectorAll('.box')
 const theIcon = document.querySelector('.the-icon')
-const svgIcon = 'svg-icon'
+let svgIcon = 'svg-icon'
 const faCircle = 'fa-circle'
 const restart = document.getElementById('restart')
 const winClass = 'won'
 const loseClass = 'lost'
+const jepai = document.getElementById('jepai')
+const medium = document.getElementById('medium')
+const impossible = document.getElementById('impossible')
 
 let arr = [0,1,2,3,4,5,6,7,8]
 let win = [
@@ -22,16 +25,25 @@ let win = [
 let botCollected = []
 let playerCollected = []
 
-let u = 0;
+// let yCond = '';
+
+let count = 0;
 let pis; 
 let won = [];
+let xTriggered = 0;
+let positionOfI = 0;
+
 
 let wonArrays = win.some(combination => {
     return combination.every(index => playerCollected.includes(index))
 })
 
-restart.onclick = () => {
-    u = 0
+let georgeStatus = ''
+let y
+
+const restartGame = () => {
+    count = 0
+    xTriggered = 0
     won = []
     arr = [0,1,2,3,4,5,6,7,8]
     botCollected = []
@@ -49,26 +61,99 @@ restart.onclick = () => {
         if (cell.classList.contains(loseClass)) {
             cell.classList.remove(loseClass)
         }
+        if (cell.classList.contains('draw')) {
+            cell.classList.remove('draw')
+        }
     })
-    console.log(`${won} and ${u}`);
-}
-
-const botAdd = (number) => {
-  container.children.item(number).children.item(1).classList.add(faCircle);
-  botCollected.push(number)
-  pis = arr.indexOf(number)
-  arr.splice(pis,1)
+    // console.log(`${won} and ${u} yay`);;
 };
 
 const findWin = arrayP => {
-    for (i=0; i<win.length; i++){
-      if (win[i].every(index => arrayP.includes(index))) {
-          won.push(win[i])
-      }
+    for (iFindWin=0; iFindWin<win.length; iFindWin++){
+    if (win[iFindWin].every(index => arrayP.includes(index))) {
+        won.push(win[iFindWin])
+    }
     };
 };
 
-const startRandomGame = () => {
+const oneTriggerAway = purpose => {
+    let decision = []
+    if (purpose === 'win the game'){
+        for (u = 0; u < win.length; u++){ 
+            for (j = 0; j < win[u].length; j++){ 
+                for (i = 0; i < botCollected.length; i++){ 
+                    // j < 3 or j < win[u].length
+                    if (win[u][j] === botCollected[i]) {
+                        decision.push(botCollected[i])
+                        positionOfI = win[u].indexOf(botCollected[i])
+                        win[u].splice(positionOfI,1)
+                    }
+                    if (win[u].length === 1) {
+                        decision.forEach(n => {
+                            win[u].push(n)
+                        })
+                        decision = []
+                        if (arr.includes(win[u][0])){
+                            return win[u][0]
+                        } else {
+                            continue
+                        }
+                    } else if (j === 2 && decision.length < 2){
+                        decision.forEach(n => {
+                            win[u].push(n)
+                        })
+                        decision = []
+                    }
+                    // console.log(`${decision} is decision and ${win[u]} is current win pattern and ${j} is j`);
+                };
+
+            }
+        };
+    } else if (purpose === 'defend'){
+        for (u = 0; u < win.length; u++){ 
+            for (j = 0; j < win[u].length; j++){ 
+                for (i = 0; i < playerCollected.length; i++){ 
+                    // j < 3 or j < win[u].length
+                    if (win[u][j] === playerCollected[i]) {
+                        decision.push(playerCollected[i])
+                        positionOfI = win[u].indexOf(playerCollected[i])
+                        win[u].splice(positionOfI,1)
+                    }
+                    if (win[u].length === 1) {
+                        decision.forEach(n => {
+                            win[u].push(n)
+                        })
+                        decision = []
+                        if (arr.includes(win[u][0])){
+                            return win[u][0]
+                        } else {
+                            continue
+                        }
+                    } else if (j === 2 && decision.length < 2){
+                        decision.forEach(n => {
+                            win[u].push(n)
+                        })
+                        decision = []
+                    }
+                    // console.log(`${decision} is decision`);
+                };
+
+            }
+        };
+    }
+};
+
+const botAdd = (number) => {
+    container.children.item(number).children.item(1).classList.add(faCircle);
+    botCollected.push(number)
+    pis = arr.indexOf(number)
+    arr.splice(pis,1)
+  };
+  
+  
+let difficultyStatus = ''
+
+const startGame = () => {
     box.forEach(cell => {
         cell.addEventListener('click', function() {
             if (win.some(combination => {
@@ -79,16 +164,45 @@ const startRandomGame = () => {
                 won[0].forEach(winBoxes => container.children.item(winBoxes).classList.add('lost'))
                 return
             }
-            let y
+            // let y
             let x = Array.prototype.indexOf.call(container.children,cell)
             const pos = arr.indexOf(x)
-            if (pos !== -1 && (u%2===0)) {
+            if (pos !== -1 && (count%2===0)) {
                 container.children.item(x).children.item(0).classList.add(svgIcon)
                 playerCollected.push(x)
                 arr.splice(pos, 1)
                 console.log('done ' + x + ' ' + arr);
-                u++  
-                y = arr[Math.floor(Math.random()*arr.length)]
+                count++  
+                // -------------------- test
+                let winTg = oneTriggerAway('win the game')
+                win = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]
+                let defTg = oneTriggerAway('defend')
+                win = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]
+                
+                if (winTg !== undefined) {
+                    y = winTg
+                    console.log('bot tries to win');
+                    win = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]
+                    // yCond = 'ready'
+                } else if (defTg !== undefined) {
+                    y = defTg
+                    console.log('bot tries to defend');
+                    win = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]
+                } else if (defTg === undefined){
+                    y = arr[Math.floor(Math.random()*arr.length)]
+                    console.log('bot randoms 1');
+                } 
+                // if the triggered are in reverse ie. [2,1,0], the function is not called
+                // if order of sequence is [0,1] then bot aims for [2] but if X goes anywhere
+                // that doesn't trigger the functions, bot aims for random
+                // try removing the purpose argument in oneTriggerAway
+
+
+
+                // console.log(y + ' is y');
+                // -------------------- test
+                // y = arr[Math.floor(Math.random()*arr.length)]
+                xTriggered++
             } 
             if (win.some(combination => {
                 return combination.every(index => playerCollected.includes(index))
@@ -97,16 +211,22 @@ const startRandomGame = () => {
                 findWin(playerCollected)
                 won[0].forEach(winBoxes => container.children.item(winBoxes).classList.add('won'))
                 return
-            } 
+            } else if (xTriggered === 5 && won.length === 0) {
+                box.forEach(cells => {
+                    cells.classList.add('draw')
+                })
+                return
+            }
 
             // console.log('this is y= '+ y);
             setTimeout(() => {
-                if (u % 2 !== 0 && arr.length>= 1) {
+                if (count % 2 !== 0 && arr.length>= 1) {
                     botAdd(y)
-                    u++
-                    // console.log(y + ' is y and ' + arr);
+                    count++
+                    console.log(y + ' is y and ' + arr);
                 };        
             }, 200)
+            console.log(xTriggered);
             // console.log('amount of u is ' + u);
             // console.log(`playerboxes are ${playerCollected}`);
             // console.log(`botboxes are ${botCollected}`);
@@ -114,7 +234,58 @@ const startRandomGame = () => {
     })
 };
 
-startRandomGame()
+jepai.onclick = () => {
+    if (difficultyStatus !== 'jepai') {
+        restartGame()
+        georgeStatus = 'george'
+        difficultyStatus = 'jepai'
+        startGame()
+        jepai.classList.add('jep-activated')
+        if (medium.classList.contains('med-activated')) {
+            medium.classList.remove('med-activated')
+        }
+        if (impossible.classList.contains('impos-activated')){
+            impossible.classList.remove('impos-activated')
+        }
 
-// container.childNodes[1].style.backgroundColor = 'red'
-// console.log(container.children.item(2).children.item(0));
+    }
+}
+
+medium.onclick = () => {
+    if (difficultyStatus !== 'medium') {
+        restartGame()
+        georgeStatus = ''
+        // startGame()
+        difficultyStatus = 'medium'
+        medium.classList.add('med-activated')
+        if (impossible.classList.contains('impos-activated')){
+            impossible.classList.remove('impos-activated')
+        }
+        if (jepai.classList.contains('jep-activated')){
+            jepai.classList.remove('jep-activated')
+        }
+    }
+}
+
+impossible.onclick = () => {
+    if (difficultyStatus !== 'impossible') {
+        restartGame()
+        georgeStatus = ''
+        // startGame()
+        difficultyStatus = 'impossible'
+        impossible.classList.add('impos-activated')
+        if (jepai.classList.contains('jep-activated')){
+            jepai.classList.remove('jep-activated')
+        }
+        if (medium.classList.contains('med-activated')) {
+            medium.classList.remove('med-activated')
+        }
+
+    }
+}
+
+restart.onclick = () => {
+    restartGame()
+}
+
+
